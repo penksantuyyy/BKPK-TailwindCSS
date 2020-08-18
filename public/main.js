9dac35d648167bcdd4ba3d099e2271b587d2bac7
penksantuyyy <penksantuyyy@gmail.com>
2020-08-18 17:46:50 +0800

    push kedua

--- public/main.js
+++ public/main.js
@@ -3,7 +3,19 @@ const showCategory = document.querySelector('#showCategory');
 const closeCategory = document.querySelector('#closeCategory');
 
 showCategory.addEventListener('click', (e) => {
-  categoryDrowpdown.classList.remove('hidden');
+  if (showCategory) {
+    showCategory.classList.add('hidden');
+    categoryDrowpdown.classList.remove('hidden');
+    closeCategory.classList.remove('hidden');
+  } 
+  e.preventDefault();
+});
 
+closeCategory.addEventListener('click', (e) => {
+  if(closeCategory) {
+    showCategory.classList.remove('hidden');
+    categoryDrowpdown.classList.add('hidden');
+    closeCategory.classList.add('hidden');
+  }
   e.preventDefault();
 });
